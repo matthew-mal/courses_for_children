16c7b2126d6deaf96c3ab4d446995fe36b7bd9eb
mat_mal <matvey_maljanov@rambler.ru>
2023-05-05 12:26:20 +0300

    Перенес все в postgres, логин работает, есть страница аккаунта

--- site1/settings.py
+++ site1/settings.py
@@ -66,11 +66,14 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'site_db',
+        'USER': 'admin',
+        'PASSWORD': 'ghbdtn01',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
